a38df4101ef4c36a69a78a866b148b059be482f2
Shoshi <s0556722535@gmail.com>
2024-06-16 01:55:59 +0300

    /corrcting mistakes in signin-signup and add add businessModel meetingModel serviceModel

--- node-server/src/routes/userRoute.ts
+++ node-server/src/routes/userRoute.ts
@@ -1,44 +1,48 @@
 import express from 'express';
-import { signUp, signIn } from '../services/userService';
+import {post_signin,post_signup } from '../controllers/userController';
 
 const router = express.Router();
 
 /**
  * @swagger
- * /signup:
+ * /users/signup:
  *   post:
  *     summary: Register a new user
  *     description: Endpoint for user registration
+ *     tags: [Authentication]
  *     requestBody:
+ *       required: true
  *       content:
  *         application/json:
  *           schema:
  *             $ref: '#/components/schemas/User'
  *     responses:
- *       '200':
+ *       '201':
  *         description: Successfully registered
  *       '400':
  *         description: Bad request
  */
-router.post('/signup', signUp);
+router.post('/signup',post_signup);
 
-/**
- * @swagger
- * /signin:
- *   post:
- *     summary: User sign-in
- *     description: Endpoint for user sign-in
- *     requestBody:
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/LoginCredentials'
- *     responses:
- *       '200':
- *         description: Successfully signed in
- *       '401':
- *         description: Unauthorized
- */
-router.post('/signin', signIn);
+// /**
+//  * @swagger
+//  * /users/signin:
+//  *   post:
+//  *     summary: User sign-in
+//  *     description: Endpoint for user sign-in
+//  *     tags: [Authentication]
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             $ref: '#/components/schemas/LoginCredentials'
+//  *     responses:
+//  *       '200':
+//  *         description: Successfully signed in
+//  *       '401':
+//  *         description: Unauthorized
+//  */
+router.post('/signin', post_signin);
 
 export default router;
