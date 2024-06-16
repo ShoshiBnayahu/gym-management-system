172495f9bf3fd8d169cdcae592a96242041824fb
Shoshi <s0556722535@gmail.com>
2024-06-16 10:50:19 +0300

    Changing the schema to be in a separate json file

--- node-server/src/middlewares/swaggerMiddleware.ts
+++ node-server/src/middlewares/swaggerMiddleware.ts
@@ -1,6 +1,10 @@
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const schemas = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../schemas.json'), 'utf8'));
 
 const options = {
   definition: {
@@ -11,24 +15,10 @@ const options = {
       description: 'API documentation using Swagger',
     },
     components: {
-      schemas: {
-        User: {
-          type: 'object',
-          properties: {
-            username: { type: 'string' },
-            email: { type: 'string', format: 'email' },
-            password: { type: 'string' },
-            role: {
-              type: 'string',
-              enum: ['admin', 'user'],
-            },
-          },
-          required: ['username', 'email', 'password', 'role'],
-        },
-      },
+      schemas, 
     },
   },
-  apis: ['./routes/*.ts'],
+  apis: ['./routers/*.ts'],
 };
 
 const specs = swaggerJSDoc(options);
